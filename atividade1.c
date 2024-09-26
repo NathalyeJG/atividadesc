ffbd26d52257c8a3316d2115e3a163a781dfbb13
Nathalye jessica <rafaelamamaeteama@gmail.com>
2024-09-26 13:39:26 +0000

    Cliente.h

---
+++ atividade1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(){
+
+    int  soma, subtracao, multiplicacao, divisao, num1, num2;
+
+
+
+
+    printf("Digite o Valor desejado:\n ");
+    scanf("%d ",&num1);
+
+    printf("Digite o Valor de numero dois:\n");
+    scanf("%d",&num2);
+
+soma = num1 + num2;
+subtracao = num1 - num2;
+multiplicacao = num1 * num2;
+divisao = num1 / num2;
+
+printf(" O resultado da soma de %d com  %d é igual %d:\n", n1, n2, soma);
+printf(" O resultado da subtracão de %d com %d é igual %d:\n, n1, n2, subtração ");
+printf(" O resultado da multiplica de %d com %d é igual %d:\n, n1, n2, multiplicação ");
+printf(" O resultado da divisão de %d com %d é igual %d:\n, n1, n2, divisão ");
+
+
+return 0;
+    
+}
